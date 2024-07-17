e832520f36ead5ed8e55b51a6cf8301b159dab33
Jean Pierre <jptr2189@gmail.com>
2024-07-17 10:02:12 -0300

    feat: Adiciona tela de avaliações

--- MoviesApp/Movies/Controllers/ReviewsListViewController.swift
+++ MoviesApp/Movies/Controllers/ReviewsListViewController.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import UIKit
+
+class ReviewsListViewController: UIViewController {
+    
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    
+    override func viewDidLoad() {
+        addRateOptions()
+    }
+    
+    private func addRateOptions() {
+        segmentedControl.removeAllSegments()
+        
+        for (index, option) in RateOptions.allCases.enumerated() {
+            segmentedControl.insertSegment(withTitle: option.rawValue, at: index, animated: false)
+        }
+        
+        segmentedControl.removeSegment(at: 0, animated: false)
+        
+        segmentedControl.insertSegment(withTitle: "All", at: 0, animated: false)
+    }
+    
+    
+    @IBAction func filterTapped(_ sender: Any) {
+        
+    }
+    
+    
+    
+    
+}
